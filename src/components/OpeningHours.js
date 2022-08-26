787a517f0609c7b9117126a30af5359022a2a22d
Dom <dom@MacBook-Pro-von-Dom.local>
2022-08-26 16:17:29 +0200

    updated packages, changes png to jpeg, fixed opening hours.

--- src/components/OpeningHours.js
+++ src/components/OpeningHours.js
@@ -10,22 +10,29 @@ const OpeningHours = () => {
             <div className="container">
                 <div className="days">
                     <ul className="openinghours">
-                        <li><strong>Montag </strong></li>
-                        <li><strong>Dienstag </strong></li>
-                        <li><strong>Mittwoch </strong></li>
-                        <li><strong>Freitag </strong></li>
-                        <li><strong>Samstag </strong></li>
-                        <li><strong>Sonntag </strong></li>
+                        <li style={{height: '45px'}}><strong>Montag</strong></li>
+                        <li style={{height: '45px'}}><strong>Dienstag</strong></li>
+                        <li style={{height: '30px'}}><strong>Mittwoch</strong></li>
+                        <li style={{height: '45px'}}><strong>Donnerstag</strong></li>
+                        <li style={{height: '45px'}}><strong>Freitag</strong></li>
+                        <li style={{height: '45px'}}><strong>Samstag</strong></li>
+                        <li><strong>Sonntag</strong></li>
                     </ul>
                 </div>
                 <div className="time">
                     <ul className="openinghours">
-                        <li>12:00 - 20:00h</li>
-                        <li>12:00 - 20:00h</li>
-                        <li>Ruhetag</li>
-                        <li>12:00 - 20:00h</li>
-                        <li>12:00 - 20:00h</li>
-                        <li>Ruhetag</li>
+                        <li style={{height: '45px'}}>11:30 - 14:30 Uhr<br />
+                        17:00 - 21:00 Uhr</li>
+                        <li style={{height: '45px'}}>11:30 - 14:30 Uhr<br />
+                            17:00 - 21:00 Uhr</li>
+                        <li style={{height: '30px'}}>Ruhetag</li>
+                        <li style={{height: '45px'}}>11:30 - 14:30 Uhr<br />
+                            17:00 - 21:00 Uhr</li>
+                        <li style={{height: '45px'}}>11:30 - 14:30 Uhr<br />
+                            17:00 - 21:00 Uhr</li>
+                        <li style={{height: '45px'}}>11:30 - 14:30 Uhr<br />
+                            17:00 - 21:00 Uhr</li>
+                        <li>16:00 - 21:00 Uhr</li>
                     </ul>
                 </div>
             </div>
--- src/components/Video.js
+++ src/components/Video.js
@@ -5,35 +5,35 @@ import './slider.css';
 const Video = () => {
     const data = [
         {
-            image: "../assets/athen01.png",
-            alt: "Außenbereich Grill Athen mit Sitzmöglichkeiten",
-            caption: `<div>
-                        Nehmen Sie in unserem Außenbereich platz
-                        </div>`
-        },
-        {
-            image: "../assets/athen02.png",
-            alt: "Eingang mit Empfang",
-            caption: `<div>
-                        Wir heißen Sie herzlich Willkommen in unserem Restaurant innen Bereich
-                        </div>`
-        },
-        {
-            image: "../assets/athen03.png",
-            alt: "Offene Küche mit Grillstation",
-            caption: `<div>
-                        Egal ob knuspriges Gyros oder leckere Spezialitäten vom Grill. Für jeden Etwas dabei!
-                        </div>`
-        },
-        {
-            image: "../assets/athen04.png",
+            image: "../assets/athen04.jpg",
             alt: "Offene Küche mit gefüllter Salattheke",
             caption: `<div>
                         Wir verwöhnen Sie mit frischen, hausgemachten Salaten. Immer knackfrisch!
                         </div>`
         },
         {
-            image: "../assets/athen05.png",
+            image: "../assets/athen01.jpg",
+            alt: "Außenbereich Grill Athen mit Sitzmöglichkeiten",
+            caption: `<div>
+                        Nehmen Sie in unserem Außenbereich platz
+                        </div>`
+        },
+        {
+            image: "../assets/athen02.jpg",
+            alt: "Eingang mit Empfang",
+            caption: `<div>
+                        Wir heißen Sie herzlich Willkommen in unserem Restaurant-Innenbereich
+                        </div>`
+        },
+        {
+            image: "../assets/athen03.jpg",
+            alt: "Offene Küche mit Grillstation",
+            caption: `<div>
+                        Egal ob knuspriges Gyros oder leckere Spezialitäten vom Grill. Für jeden etwas dabei!
+                        </div>`
+        },
+        {
+            image: "../assets/athen05.jpg",
             alt: "Sitzmöglichkeiten im Innenbereich",
             caption: `<div>
                         Auch im Innenbereich halten wir Lieblingsplätze bereit für Sie. 
@@ -57,7 +57,7 @@ const Video = () => {
             height="500px"
             captionStyle={captionStyle}
             radius="0"
-            slideNumber={true}
+            slideNumber={false}
             slideNumberStyle={slideNumberStyle}
             captionPosition="bottom"
             automatic={true}
